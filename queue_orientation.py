9c53e31fd30562fc7b32fb38ad4faaef55714eaa
e1m1m1a <emma.pruvost11@gmail.com>
2023-04-28 16:42:54 +0900

    few corrections to queue_orientation.py and plotting function added to queue_prediction.py

--- queue_orientation.py
+++ queue_orientation.py
@@ -4,41 +4,52 @@ import cv2
 import math
 
 
-def find_people_in_queue(bounding_boxes):
+def find_people_in_queue(bounding_boxes, threshold=2):
 # bounding_boxes is an array of arrays, each array containing multiple (x,y) points representing the center of each bounding boxe  
 
-    bounding_boxes = bounding_boxes[:1] # just to reduce nb of instance for testing
+# threshold: Another approach is to use a statistical method, such as calculating the mean and standard deviation of the distances between the convex hull line and the mid-points of the bounding boxes, and then setting the threshold as a multiple of the standard deviation (e.g. 2 or 3 times the standard deviation).
+        
+    #bounding_boxes = bounding_boxes[:1] # just to reduce nb of instance for testing
+
+    instances = []
 
     for instance in bounding_boxes:
-        # Define the input points
-        points = np.array(instance, np.float32)
 
-        # Compute the convex hull
-        hull = cv2.convexHull(points)
+        if len(instance) == 0: # no people in the image
+            instances.append([[],[],[],[]])
 
-        # Extract the x and y coordinates of the points on the convex hull
-        hull_points = []
-        for i in range(len(hull)):
-            hull_points.append((hull[i][0][0], hull[i][0][1]))
+        else:
 
-        # Find the line that minimizes the perpendicular distance of the points on the hull
-        x_coords, y_coords = zip(*hull_points)
-        A = np.vstack([x_coords, np.ones(len(x_coords))]).T
-        m, c = np.linalg.lstsq(A, y_coords, rcond=None)[0]
-        line_points = [(min(x_coords), m*min(x_coords) + c),
-                    (max(x_coords), m*max(x_coords) + c)]
+            # Define the input points
+            points = np.array(instance, np.float32)
 
-        threshold = 2 # Another approach is to use a statistical method, such as calculating the mean and standard deviation of the distances between the convex hull line and the mid-points of the bounding boxes, and then setting the threshold as a multiple of the standard deviation (e.g. 2 or 3 times the standard deviation).
-        pts_inside_hull = []
-        pts_outside_hull = []
-        for point in points:
-            distance = abs(m*point[0] - point[1] + c) / math.sqrt(m**2 + 1)
-            if distance <= threshold:
-                pts_inside_hull.append(point)
-            else:
-                pts_outside_hull.append(point)
-            
-        return pts_inside_hull, pts_outside_hull, hull_points, line_points
+            # Compute the convex hull
+            hull = cv2.convexHull(points)
+
+            # Extract the x and y coordinates of the points on the convex hull
+            hull_points = []
+            for i in range(len(hull)):
+                hull_points.append((hull[i][0][0], hull[i][0][1]))
+
+            # Find the line that minimizes the perpendicular distance of the points on the hull
+            x_coords, y_coords = zip(*hull_points)
+            A = np.vstack([x_coords, np.ones(len(x_coords))]).T
+            m, c = np.linalg.lstsq(A, y_coords, rcond=None)[0]
+            line_points = [(min(x_coords), m*min(x_coords) + c),
+                        (max(x_coords), m*max(x_coords) + c)]
+
+            pts_inside_hull = []
+            pts_outside_hull = []
+            for point in points:
+                distance = abs(m*point[0] - point[1] + c) / math.sqrt(m**2 + 1)
+                if distance <= threshold:
+                    pts_inside_hull.append(point)
+                else:
+                    pts_outside_hull.append(point)
+
+            instances.append([pts_inside_hull, pts_outside_hull, hull_points, line_points])
+                
+    return instances
         
 
 def plot(pts_inside_hull, pts_outside_hull, hull_points, line_points):
--- queue_prediction.py
+++ queue_prediction.py
@@ -1,8 +1,44 @@
-import os, sys
+import os, sys, cv2
 import matplotlib.pyplot as plt
+import numpy as np
 sys.path.append(os.getcwd()+'/models/faster_rcnn')
 from models.faster_rcnn import inference
 from queue_orientation import *
+
+
+def plot_image_with_points(image_path, points, saving_folder=None):
+    image_name = image_path.split("/")[-1]
+
+    # Load the image using cv2
+    image = cv2.imread(image_path)
+
+    # Convert the image from BGR to RGB format
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Convert the list of points to a numpy array and extract the x and y coordinates
+    points = np.array(points)
+    x_coords, y_coords = np.hsplit(points, 2)
+
+    # Create a new figure and set its size
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    # Plot the image
+    ax.imshow(image)
+
+    # Plot the points as red circles with a radius of 5 pixels
+    ax.scatter(x_coords, y_coords, s=5**2, c='r')
+
+    # Set the x and y limits to match the image dimensions
+    ax.set_xlim(0, image.shape[1])
+    ax.set_ylim(image.shape[0], 0)
+
+    # Save image
+    if saving_folder is not None:
+        plt.savefig(saving_folder + "/" + image_name, image)
+
+    # Show the plot
+    plt.show()
+
 
 
 directory = os.getcwd() + "/models/faster_rcnn/images"
@@ -15,8 +51,17 @@ out = faster_rcnn.run_inference()
 
 points = faster_rcnn.get_bounding_boxes_centers(out)
 
-people_in_queue, _, _, _ = find_people_in_queue(points)
+people_in_queue = find_people_in_queue(points)
 
-number_people_per_image = [len(p) for p in people_in_queue]
+number_people_per_image = [len(p[0]) for p in people_in_queue]
 
 print(number_people_per_image)
+
+i = 0
+for im_name in os.listdir(directory):
+    plot_image_with_points(directory + "/" + im_name, people_in_queue[i][0])
+    i += 1
+
+
+
+
